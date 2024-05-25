9dc9c5085ba070b85314582a31fab3766463a2b4
Jeremias Ferrao <feronest@yahoo.com>
2024-05-25 23:34:08 +0200

    Semi working docker approach

--- code_evaluator/evaluate_code.py
+++ code_evaluator/evaluate_code.py
@@ -4,23 +4,35 @@ def evaluate_code(code, command, script):
     client = docker.from_env()
     
     # Create a volume
-    volume = client.volumes.create(name="code_evaluator")
+    volume = client.volumes.create()
     
-    # Create a container with the volume and memory limit
-    container = client.containers.run("python:3.8", detach=True, volumes={volume.name: {'bind': '/mnt/volume', 'mode': 'rw'}}, mem_limit="512m")
+    code_file_creation_command = f'echo "{code}" > main.txt'
     
-    # Execute the code
-    output = container.exec_run(command)
-    print(output)
+    print(code_file_creation_command)
     
-    # Stop and remove the container
-    container.stop()
-    container.remove()
+    # Add code to the volume
+    code_creation = client.containers.run("debian", command=code_file_creation_command, volumes={volume.name: {'bind': '/mnt/volume', 'mode': 'rw'}}, mem_limit="512m", working_dir="/mnt/volume", privileged=True)
     
-    # Remove the volume
-    volume.remove()
+    print("File: ", code_creation)
+    
+    output = client.containers.run("debian", command='ls', volumes={volume.name: {'bind': '/mnt/volume', 'mode': 'rw'}}, mem_limit="512m", working_dir="/mnt/volume")
+    
+    print("Output: ", output)
+    
+    # Persistance technique
+    persistance_success = client.containers.run("debian", command=command, volumes={volume.name: {'bind': '/mnt/volume', 'mode': 'rw'}}, mem_limit="512m", working_dir="/mnt/volume", privileged=True)
+    
+    print("Persistance: ", persistance_success)
+    
+    # reboot the container
+    client.containers.prune()
+    
+    # Check if the data was saved
+    output = client.containers.run("debian", command='ls', volumes={volume.name: {'bind': '/mnt/volume', 'mode': 'rw'}}, mem_limit="512m", working_dir="/mnt/volume")
+    
+    print("Output: ", output)
     
     return True
 
 if __name__ == "__main__":
-    evaluate_code("", "ls", "default")
+    evaluate_code("", "touch hello.txt", "default")
