71fb707e0eff6506c0708eadf5170897bcaaf54d
Jeremias Ferrao <feronest@yahoo.com>
2024-05-25 21:11:28 +0200

    Not working container

--- code_evaluator/evaluate_code.py
+++ code_evaluator/evaluate_code.py
@@ -1,2 +1,26 @@
+import docker
+
 def evaluate_code(code, command, script):
+    client = docker.from_env()
+    
+    # Create a volume
+    volume = client.volumes.create(name="code_evaluator")
+    
+    # Create a container with the volume and memory limit
+    container = client.containers.run("python:3.8", detach=True, volumes={volume.name: {'bind': '/mnt/volume', 'mode': 'rw'}}, mem_limit="512m")
+    
+    # Execute the code
+    output = container.exec_run(command)
+    print(output)
+    
+    # Stop and remove the container
+    container.stop()
+    container.remove()
+    
+    # Remove the volume
+    volume.remove()
+    
     return True
+
+if __name__ == "__main__":
+    evaluate_code("", "ls", "default")
