b17aac096261bb076befc26ba8fdfa224f458f35
Davide_Zani <davide.zani@full-orbit.com>
2024-05-26 20:16:42 +0200

    resetting machine and working connection made

--- workingConnection.py
+++ workingConnection.py
@@ -7,7 +7,7 @@ def ssh_connect_and_execute_command(hostname, port, username, key_file_path, com
         "instance/HackathonTinkering.pem")
     client = paramiko.SSHClient()
     client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    client.connect(hostname='13.49.221.6', username="ubuntu", pkey=k, allow_agent=False, look_for_keys=False)
+    client.connect(hostname='3.70.158.5', username="ubuntu", pkey=k, allow_agent=False, look_for_keys=False)
 
     # Execute a command and capture the output
     stdin, stdout, stderr = client.exec_command(command)
@@ -28,14 +28,10 @@ port = 22
 username = 'ec2-user'
 key_file_path = ('/home/davidez/Nextcloud/THE PROJECTS FOLDER/AISIG/AIEvaluations Hackathon/Hackathon Tinkering EC2 '
                  'instance/public.pub')
-command = 'ls -la'
 
-if __name__ == "__main__":
 
-    # Call the function
-    stdout, stderr = ssh_connect_and_execute_command(hostname, port, username, key_file_path, command)
+def evaluate_code(command):
+    return ssh_connect_and_execute_command(hostname, port, username, key_file_path, command)
 
-    print("Standard Output:")
-    print(stdout)
-    print("Standard Error:")
-    print(stderr)
+
+print(evaluate_code("echo 'Hello World'"))
